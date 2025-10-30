28f65b07eac49ef5e0a312818737f90200e4da66
abroraxatov1 <abroraxatov1@gmail.com>
2025-10-30 20:51:19 +0500

    Update ekinAI.py

--- ekinAI.py
+++ ekinAI.py
@@ -222,6 +222,16 @@ st.markdown("""
         justify-content: center;
         align-items: center;
     }
+    .author {
+        text-align: center;
+        color: #005454;
+        font-size: 1rem;
+        margin-top: 2rem;
+        padding: 1rem;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
     .copyright img {
         margin-right: 10px;
         width: 50px; /* Adjustable logo size */
@@ -650,7 +660,8 @@ else:
 
     st.markdown(f'<div class="copyright"> <a href="https://www.samdu.uz/uz" class="samdu"> Samarqand davlat universiteti  </a> <a href="http://ai-lab.sampc.uz/" class="lab"> "Sun\'iy intellekt labaratoriyasi" </a> tomonidan ishlab chiqilgan. © 2025 <span class="ekin2">Ekin</span> <span class="ai2"> AI</span>.</div>', unsafe_allow_html=True)
 
-st.markdown(f'<div class="copyright">  Muallif: <a href="https://t.me/axatov_a" class="lab"> Axatov Abror </a> <span class="ekin2">Tel: </span> <span class="ai2">+998(99)-590-52-65</span>.</div>', unsafe_allow_html=True)
+st.markdown(f'<div class="author">  Muallif: <a href="https://t.me/axatov_a" class="lab"> Axatov Abror </a> <span class="ekin2">Tel: </span> <span class="ai2">+998(99)-590-52-65</span>.</div>', unsafe_allow_html=True)
+
 
 
 
